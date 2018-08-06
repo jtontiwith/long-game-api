82a93df5b3ed7fc19a06b3ba00edc49824854002
jtontiwith <jtonti@gmail.com>
2018-08-06 10:11:07 -0500

    configure remote db

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 "use strict";
-exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/longGameDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://localhost/longGameDB";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/longGameDB";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/longGameTestDB";
 exports.PORT = process.env.PORT || 8080;
+
+
+
+//mongodb://lgAdmin:LgadminlG1234@ds141621.mlab.com:41621/long-game-db
+
+//mongo ds141621.mlab.com:41621/long-game-db -u lgAdmin -p LgadminlG1234
+

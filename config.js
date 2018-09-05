aa81e74ce3be30f5781aaaa3919ae48713275e70
jtontiwith <jtonti@gmail.com>
2018-09-05 11:03:56 -0500

    redeploy

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
+
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || "mongodb://localhost/longGameDB";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/longGameTestDB";
 exports.PORT = process.env.PORT || 8080;
